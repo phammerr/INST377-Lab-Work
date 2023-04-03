587a95b2d22a73f736a3ca2f5f8a99a79ff8bb58
phammerr <ethananpham@gmail.com>
2023-04-02 23:09:59 -0400

    hid generate button

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -46,10 +46,13 @@ function getRandomIntInclusive(min, max) {
     const filterDataButton = document.querySelector('#filter');// Add a querySelector that targets your filter button here
     const loadDataButton = document.querySelector('#data_load');
     const generateListButton = document.querySelector('#generate');
-    
+    const textField = document.querySelector('#resto');
+
     const loadAnimation = document.querySelector('#data_load_animation');
     loadAnimation.style.display = 'none';
-  
+    generateListButton.classList.add('hidden');
+
+    let storedList =[];
     let currentList = []; // this is "scoped" to the main event function
     
     /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
@@ -65,10 +68,13 @@ function getRandomIntInclusive(min, max) {
       const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
   
       // This changes the response from the GET into data we can use - an "object"
-      currentList = await results.json();
+      storedList = await results.json();
+      if (storedList.length > 0) {
+        generateListButton.classList.remove('hidden');
+      }
   
       loadAnimation.style.display = 'none';
-      console.table(currentList); 
+      console.table(storedList); 
     });
   
     filterDataButton.addEventListener('click', (event) => {
@@ -87,24 +93,18 @@ function getRandomIntInclusive(min, max) {
   
     generateListButton.addEventListener('click', (event) => {
       console.log('generate new list');
-      const restaurantsList = cutRestaurantList(currentList);
-      injectHTML(restaurantsList);
+      currentList = cutRestaurantList(storedList);
+      console.log(currentList)
+      injectHTML(currentList);
     });
+
+    textField.addEventListener('input', (event) => {
+      console.log('input', event.target.value);
+      const newList = filterList(currentList, event.target.value);
+      injectHTML(newList);
+      console.log(newList);
+    })
   
-    /*
-      Now that you HAVE a list loaded, write an event listener set to your filter button
-      it should use the 'new FormData(target-form)' method to read the contents of your main form
-      and the Object.fromEntries() method to convert that data to an object we can work with
-      When you have the contents of the form, use the placeholder at line 7
-      to write a list filter
-      Fire it here and filter for the word "pizza"
-      you should get approximately 46 results
-    */
   }
   
-  /*
-    This adds an event listener that fires our main event only once our page elements have loaded
-    The use of the async keyword means we can "await" events before continuing in our scripts
-    In this case, we load some data when the form has submitted
-  */
   document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
